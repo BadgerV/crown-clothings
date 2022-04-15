6f9293633fb72770606c36106a4e08924a246eae
unknown <segunfaozan112@gmail.com>
2022-04-15 02:41:47 -0700

    added the dropdown component functionality

---
+++ src/components/button/nnesenes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import dp from '../../assets/crown.svg';
+
+
+const example = () => {
+    return(
+        <div
+            className='div'
+            style = {{
+                backgroundImage : `url${dp}`
+            }}
+        ></div>
+    )
+}
+
+export default example
